23203e645b9511c824e4a82b4ceae689a75ed3c1
diego22655 <diegozapata2000@gmail.com>
2024-03-14 01:02:20 -0500

    v2

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('facebookBtn').addEventListener('click', function() {
+    // Aquí puedes colocar el código para la autenticación con Facebook
+    // Por ejemplo, redireccionar a la página de autenticación de Facebook
+    // window.location.href = 'URL_DE_AUTENTICACION_DE_FACEBOOK';
+    alert('Aquí iría la lógica de autenticación con Facebook');
+});
