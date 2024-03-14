2389f0b3edc773cfd9d060dd05f61fdbe69561b3
diego22655 <diegozapata2000@gmail.com>
2024-03-14 01:31:02 -0500

    v3

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 document.getElementById('facebookBtn').addEventListener('click', function() {
-    // Aquí puedes colocar el código para la autenticación con Facebook
-    // Por ejemplo, redireccionar a la página de autenticación de Facebook
-    // window.location.href = 'URL_DE_AUTENTICACION_DE_FACEBOOK';
-    alert('Aquí iría la lógica de autenticación con Facebook');
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '7590829360956820',
+          cookie     : true,
+          xfbml      : true,
+          version    : 'v12.0'
+        });
+          
+        FB.AppEvents.logPageView();   
+          
+      };
+    
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+       console.log()
+    //window.location.href = 'https://connect.facebook.net/en_US/sdk.js';
+    //alert('Aquí iría la lógica de autenticación con Facebook');
 });
